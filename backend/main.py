985200e9d89a1da308d74cb76ec3cb235a0e7513
Shalha-Mucha18 <shalha.mucha@gmail.com>
2025-10-05 13:28:45 +0600

    Restore backend folder

---
+++ backend/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import story, job
+from core.config import setting
+from db.database import create_tables
+from models import story as story_models, job as job_models
+from db.database import create_tables
+
+create_tables()
+
+app = FastAPI(
+   
+   title = "Choose Your Own Adventure Game API",
+   descrption = "api to generate cool stories",
+   version = "0.1.0",
+   docs_url = "/docs",
+   redoc_url = "/redoc"
+
+
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(story.router,prefix=setting.API_PREFIX)
+app.include_router(job.router, prefix=setting.API_PREFIX)
+
+@app.on_event("startup")
+def startup_event():
+    create_tables()
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, port=8000,reload = True)
+
+
